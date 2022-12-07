4c242473f1e6a37be680db99e7dab51ed9c023e7
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-12-08 01:55:27 +0600

    products and prices is showing on cart

--- shopping.js
+++ shopping.js
@@ -1,6 +1,30 @@
+const productInfo = [];
+
+function showProduct(cart) {
+  const productRow = document.getElementById('product-row');
+  productRow.innerHTML = ``;
+
+  for (i = 0; i < cart.length; i++) {
+    console.log(cart[i].productName, cart[i].productPrice);
+    const tr = document.createElement('tr');
+    // tr.innerHTML = ``;
+    tr.innerHTML = `
+            <th>${i + 1}</th>
+            <th>${cart[i].productName}</th>
+            <th>${cart[i].productPrice}</th>
+    `
+    productRow.appendChild(tr);
+  }
+
+}
+
 function shoppingCart(element) {
   let products = element.parentNode.children[0].innerText;
   let prices = element.parentNode.children[1].children[0].innerText;
-  console.log(products, prices)
-
+  const productObj = {
+    productName: products,
+    productPrice: prices
+  }
+  productInfo.push(productObj);
+  showProduct(productInfo);
 }
