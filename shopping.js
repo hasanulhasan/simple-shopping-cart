24d2a5b370d02973b1c3630ae3bd73efee1357c7
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-12-08 10:40:23 +0600

    total price is working good

--- shopping.js
+++ shopping.js
@@ -3,19 +3,28 @@ const productInfo = [];
 function showProduct(cart) {
   const productRow = document.getElementById('product-row');
   productRow.innerHTML = ``;
+  let totalPrice = 0;
 
   for (i = 0; i < cart.length; i++) {
-    console.log(cart[i].productName, cart[i].productPrice);
+    const name = cart[i].productName;
+    const price = cart[i].productPrice;
     const tr = document.createElement('tr');
-    // tr.innerHTML = ``;
+    totalPrice = totalPrice + price;
     tr.innerHTML = `
             <th>${i + 1}</th>
-            <th>${cart[i].productName}</th>
-            <th>${cart[i].productPrice}</th>
+            <th>${name}</th>
+            <th>${price}</th>
     `
     productRow.appendChild(tr);
-  }
 
+  }
+  const tr = document.createElement('tr');
+  tr.innerHTML = `
+            <th colspan="2">${cart.length} Items price Total</th>
+            <th>${totalPrice}</th>
+  `
+  productRow.appendChild(tr);
+  console.log(totalPrice);
 }
 
 function shoppingCart(element) {
@@ -23,7 +32,7 @@ function shoppingCart(element) {
   let prices = element.parentNode.children[1].children[0].innerText;
   const productObj = {
     productName: products,
-    productPrice: prices
+    productPrice: parseFloat(prices)
   }
   productInfo.push(productObj);
   showProduct(productInfo);
